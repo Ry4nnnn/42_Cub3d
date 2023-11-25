038043165f79e199cc1ae6aaff3cce3a463f6884
Thong Zi Qi <ziqi.thong@gmail.com>
2023-11-25 12:55:36 +0800

    Done check textures' path and floor's and ceiling's colour

--- src/map_check.c
+++ src/map_check.c
@@ -1,70 +1,131 @@
 #include "cub3d.h"
 
-// int	init_textures(t_cub3d *data, char *str)
-// {
-// 	int w;
-// 	int h;
+int	init_textures(t_cub3d *data, char *str)
+{
+	int		w;
+	int		h;
+	char	*path;
 
-// 	w = 0;
-// 	h = 0;
-// 	(void)data;
-// 	(void)w;
-// 	(void)h;
-// 	// if (!ft_strncmp(str, "NO ", 3))
-// 	// 	data->texture.north = mlx_xpm_file_to_image(data->mlx, ft_strchr(str, '.'), &w, &h);
-// 	// else if (!ft_strncmp(str, "SO ", 3))
-// 	// 	data->texture.south = mlx_xpm_file_to_image(data->mlx, ft_strchr(str, '.'), &w, &h);
-// 	// else if (!ft_strncmp(str, "WE ", 3))
-// 	// 	data->texture.west = mlx_xpm_file_to_image(data->mlx, ft_strchr(str, '.'), &w, &h);
-// 	// else if (!ft_strncmp(str, "EA ", 3))
-// 	// 	data->texture.east = mlx_xpm_file_to_image(data->mlx, ft_strchr(str, '.'), &w, &h);
-// 	// else if (!ft_strncmp(str, "DO ", 3))
-// 	// 	data->texture.door = mlx_xpm_file_to_image(data->mlx, ft_strchr(str, '.'), &w, &h);
-// 	// data->texture.width = w;
-// 	// data->texture.height = h;
-// 	return (0);
-// }
+	if (ft_strchr(str, '.') == NULL)
+		return (1);
+	path = ft_strjoin("./images/", ft_strchr(str, '.') + 2);
+	if (!ft_strncmp(str, "NO ", 3))
+	{
+		data->texture->north->img_ptr = mlx_xpm_file_to_image(data->mlx, path, &w, &h);
+		if (data->texture->north->img_ptr == NULL)
+		{
+			free (path);
+			return (1);
+		}
+		data->texture->north->addr = mlx_get_data_addr(data->texture->north->img_ptr, &data->texture->north->bpp, &data->texture->north->line_length, &data->texture->north->endian);
+		data->texture->north->width = w;
+		data->texture->north->height = h;
+		printf ("north %i %i\n", data->texture->north->width, data->texture->north->height);
+	}
+	else if (!ft_strncmp(str, "SO ", 3))
+	{
+		data->texture->south->img_ptr = mlx_xpm_file_to_image(data->mlx, path, &w, &h);
+		if (data->texture->south->img_ptr == NULL)
+		{
+			free (path);
+			return (1);
+		}
+		data->texture->south->addr = mlx_get_data_addr(data->texture->south->img_ptr, &data->texture->south->bpp, &data->texture->south->line_length, &data->texture->south->endian);
+		data->texture->south->width = w;
+		data->texture->south->height = h;
+		printf ("south %i %i\n", data->texture->south->width, data->texture->south->height);
+	}
+	else if (!ft_strncmp(str, "WE ", 3))
+	{
+		data->texture->west->img_ptr = mlx_xpm_file_to_image(data->mlx, path, &w, &h);
+		if (data->texture->west->img_ptr == NULL)
+		{
+			free (path);
+			return (1);
+		}
+		data->texture->west->addr = mlx_get_data_addr(data->texture->west->img_ptr, &data->texture->west->bpp, &data->texture->west->line_length, &data->texture->west->endian);
+		data->texture->west->width = w;
+		data->texture->west->height = h;
+		printf ("west %i %i\n", data->texture->west->width, data->texture->west->height);
+	}
+	else if (!ft_strncmp(str, "EA ", 3))
+	{
+		data->texture->east->img_ptr = mlx_xpm_file_to_image(data->mlx, path, &w, &h);
+		if (data->texture->east->img_ptr == NULL)
+		{
+			free (path);
+			return (1);
+		}
+		data->texture->east->addr = mlx_get_data_addr(data->texture->east->img_ptr, &data->texture->east->bpp, &data->texture->east->line_length, &data->texture->east->endian);
+		data->texture->east->width = w;
+		data->texture->east->height = h;
+		printf ("east %i %i\n", data->texture->east->width, data->texture->east->height);
+	}
+	else if (!ft_strncmp(str, "DO ", 3))
+	{
+		data->texture->door->img_ptr = mlx_xpm_file_to_image(data->mlx, path, &w, &h);
+		if (data->texture->door->img_ptr == NULL)
+		{
+			free (path);
+			return (1);
+		}
+		data->texture->door->addr = mlx_get_data_addr(data->texture->door->img_ptr, &data->texture->door->bpp, &data->texture->door->line_length, &data->texture->door->endian);
+		data->texture->door->width = w;
+		data->texture->door->height = h;
+		printf ("door %i %i\n", data->texture->door->width, data->texture->door->height);
+	}
+	free (path);
+	return (0);
+}
 
-// int init_fc_color(t_cub3d *data, char *str)
-// {
-// 	int i;
+int	rgb_to_hex(int r, int g, int b)
+{
+	return ((r << 16) + (g << 8) + b);
+}
 
-// 	i = 0;
-// 	if (!ft_strncmp(str, "F ", 2))
-// 	{
-// 		data->texture.floor[0] = ft_atoi(&str[2]);
-// 		data->texture.floor[1] = ft_atoi(ft_strchr(&str[2], ',') + 1);
-// 		data->texture.floor[2] = ft_atoi(ft_strchr(ft_strchr(&str[2], ',') + 1, ',') + 1);
-// 		while (data->texture.floor[i])
-// 		{
-// 			// printf ("floor %i\n", data->texture.floor[i]);
-// 			if (data->texture.floor[i] < 0 || data->texture.floor[i] > 255)
-// 			{
-// 				printf ("invalid floor color\n");
-// 				exit (1);
-// 			}
-// 			i++;
-// 		}
-// 		i = 0;
-// 	}
-// 	else if (!ft_strncmp(str, "C ", 2))
-// 	{
-// 		data->texture.ceiling[0] = ft_atoi(&str[2]);
-// 		data->texture.ceiling[1] = ft_atoi(ft_strchr(&str[2], ',') + 1);
-// 		data->texture.ceiling[2] = ft_atoi(ft_strchr(ft_strchr(&str[2], ',') + 1, ',') + 1);
-// 		while (data->texture.ceiling[i])
-// 		{
-// 			// printf ("ceiling %i\n", data->texture.ceiling[i]);
-// 			if (data->texture.ceiling[i] < 0 || data->texture.ceiling[i] > 255)
-// 			{
-// 				printf ("invalid ceiling color\n");
-// 				exit (1);
-// 			}
-// 			i++;
-// 		}
-// 	}
-// 	return (0);
-// }
+int init_fc_color(t_cub3d *data, char *str)
+{
+	int i;
+	char **array;
+	char **num;
+
+	i = -1;
+	array = ft_split(str, ' ');
+	num = ft_split(array[1], ',');
+	if (!ft_strncmp(str, "F ", 2))
+	{
+		i = -1;
+		while (num[++i])
+		{
+			if (ft_atoi(num[i]) < 0 || ft_atoi(num[i]) > 255)
+			{
+				free_2d(array);
+				free_2d(num);
+				return (1);
+			}
+		}
+		data->texture->floor = rgb_to_hex(ft_atoi(num[0]), ft_atoi(num[1]), ft_atoi(num[2]));
+		printf ("floor %i\n", data->texture->floor);
+	}
+	else if (!ft_strncmp(str, "C ", 2))
+	{
+		i = -1;
+		while (num[++i])
+		{
+			if (ft_atoi(num[i]) < 0 || ft_atoi(num[i]) > 255)
+			{
+				free_2d(array);
+				free_2d(num);
+				return (1);
+			}
+		}
+		data->texture->ceiling = rgb_to_hex(ft_atoi(num[0]), ft_atoi(num[1]), ft_atoi(num[2]));
+		printf ("ceiling %i\n", data->texture->ceiling);
+	}
+	free_2d(array);
+	free_2d(num);
+	return (0);
+}
 
 // void init_map_layout(t_cub3d *data, char **array, int i)
 // {
@@ -190,34 +251,54 @@
 // 		printf ("%s\n", data->map[i]);
 // }
 
-// int	init_file_data(t_cub3d *data, char **array)
-// {
-// 	int i;
+int	init_file_data(t_cub3d *data, char **array)
+{
+	int i;
+	int	result;
 
-// 	i = 0;
-// 	while (array[i])
-// 	{
-// 		init_textures(data, array[i]);
-// 		init_fc_color(data, array[i]);
-// 		if (ft_strncmp(array[i], " ", 1) == 0 || ft_strncmp(array[i], "1", 1) == 0)
-// 		{
-// 			init_map_layout(data, array, i);
-// 			handle_map(data);
-// 			break;
-// 		}
-// 		else
-// 			ft_putstr_fd("Error: Invalid file format\n", 2);
-// 		i++;
-// 	}
-// 	// show_map_info(data);
-// 	return (0);
-// }
+	i = 0;
+	while (array[i])
+	{
+		if (i >= 0 && i <= 4)
+		{
+			result = init_textures(data, array[i]);
+			if (result == 1)
+			{
+				ft_putstr_fd("Error: Invalid texture\n", 2);
+				break;
+			}
+		}
+		else if (i >= 5 && i <= 6)
+		{
+			result = init_fc_color(data, array[i]);
+			if (result == 1)
+			{
+				ft_putstr_fd("Error: Invalid colour\n", 2);
+				break;
+			}
+		}
+		
+		// if (ft_strncmp(array[i], " ", 1) == 0 || ft_strncmp(array[i], "1", 1) == 0)
+		// {
+		// 	init_map_layout(data, array, i);
+		// 	handle_map(data);
+		// 	break;
+		// }
+		// else
+		// 	ft_putstr_fd("Error: Invalid file format\n", 2);
+		i++;
+	}
+	// show_map_info(data);
+	if (result == 1)
+		return (1);
+	return (0);
+}
 
 int	map_check(t_cub3d *data, char **array)
 {
 	(void)data;
 	(void)array;
-    // if(init_file_data(data, array) == 1)
-	// 	return (1);
+    if(init_file_data(data, array) == 1)
+		return (1);
 	return (0);
 }
